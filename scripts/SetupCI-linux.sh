fd160cb6b7d990cdc2af471a3ec1c8a591003746
msqr1 <134968689+msqr1@users.noreply.github.com>
2026-05-19 20:18:06 -0700

    Packaging

--- scripts/SetupCI-linux.sh
+++ scripts/SetupCI-linux.sh
@@ -1,5 +1,11 @@
 sudo apt remove -y clang-* lldb-* lld-* clang-format-* clang-tidy-* --autoremove &&
 wget https://apt.llvm.org/llvm.sh &&
 chmod u+x llvm.sh &&
-sudo ./llvm.sh 22
-sudo apt install -y libllvm22 llvm-22-dev llvm-22-tools libclang-common-22-dev libclang-22-dev libclang-cpp22-dev clang-tools-22
+ver="22"
+sudo ./llvm.sh $ver
+sudo apt install -y \
+  llvm-${ver}-dev \
+  libclang-common-${ver}-dev \
+  libclang-${ver}-dev \
+  libclang-cpp${ver}-dev \
+  clang-tools-${ver}
