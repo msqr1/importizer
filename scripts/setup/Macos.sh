299c0070c9d741971f970324e64288a01466e805
msqr1 <134968689+msqr1@users.noreply.github.com>
2026-05-27 12:37:53 -0700

    Attempt to fix MacOS CI, stop including CPM.cmake

--- scripts/setup/Macos.sh
+++ scripts/setup/Macos.sh
@@ -1,7 +1,12 @@
 set -e
-brew install llvm@22
+HOMEBREW_NO_ENV_HINTS=1 brew install llvm@22
 export PATH="$(brew --prefix llvm@22)/bin:$PATH"
 export CMAKE_PREFIX_PATH="$(brew --prefix llvm@22)"
 
+symbolizer="$(brew --prefix llvm@22)/bin/llvm-symbolizer"
+echo "$symbolizer $([ -f "$symbolizer" ] && echo "exist" || echo "doesn't exist")s"
+
+sudo ln -sf /usr/bin/atos "$symbolizer"
+
 # Fix external symbolizer not found error
-export ASAN_OPTIONS="external_symbolizer_path=$(brew --prefix llvm@22)/bin/llvm-symbolizer"
+export ASAN_OPTIONS="external_symbolizer_path=$symbolizer"
