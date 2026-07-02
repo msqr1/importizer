325cbc3aeec036410db73002034a8f0de590833c
msqr1 <134968689+msqr1@users.noreply.github.com>
2026-07-02 09:26:33 -0700

    Ready for core logic

--- scripts/PostInstall.cmake
+++ scripts/PostInstall.cmake
@@ -1,5 +1,5 @@
 # Post-installation steps
-# For use in CMake install rules, do not run alone
+# For use inclusion in CMake install rules
 
 if(LINUX)
   file(RENAME "${exe}" "${CMAKE_INSTALL_PREFIX}/importizer.bin")
@@ -15,22 +15,23 @@ if(LINUX)
   # We use string(CONCAT ) to be able to use raw strings
   string(CONCAT trampoline
 [[#!/usr/bin/env sh
-DIR="$(cd "$(dirname "$0")" && pwd)"
-exec "$DIR/]]
+dir="$(cd "$(dirname "$0")" && pwd)"
+exec "$dir/]]
   ${loaderName}
-[[" --library-path "$DIR" "$DIR/importizer.bin" "$@"]]
+[[" --library-path "$dir" "$dir/importizer.bin" "$@"]]
   )
   file(WRITE "${exe}" "${trampoline}")
 endif()
 
-# Make sure the executable lives up to its kind
-file(CHMOD "${exe}" PERMISSIONS
-  OWNER_READ
-  OWNER_WRITE
-  OWNER_EXECUTE
-  GROUP_READ
-  GROUP_EXECUTE
-  WORLD_READ
-  WORLD_WRITE
-  WORLD_EXECUTE
-)
+if(NOT WIN32)
+  file(CHMOD_RECURSE "${CMAKE_INSTALL_PREFIX}" FILE_PERMISSIONS
+    OWNER_READ
+    OWNER_WRITE
+    OWNER_EXECUTE
+    GROUP_READ
+    GROUP_EXECUTE
+    WORLD_READ
+    WORLD_WRITE
+    WORLD_EXECUTE
+  )
+endif()
